f3ba0500ae9f25b0182b661733cd55a0569320d8
Erickso-Barillas <141478209+Erickso-Barillas@users.noreply.github.com>
2026-04-26 22:54:06 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'gastos-se-v1';
+const ASSETS = [
+  './',
+  './Gastos.html',
+  './manifest.json',
+  './favicomatic/favicon-196x196.png' // Ruta actualizada
+];
+
+// Instalación: Guarda los archivos en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Estrategia: Cargar desde caché, si no hay, ir a la red
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
